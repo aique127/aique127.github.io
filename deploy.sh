36d49d5d2361f963b2624d96aac9e21d55c0732e
aique127 <840926292@qq.com>
2021-06-19 02:17:15 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
-git push;
+# git push;
 # node .bin/;
 
 npm run build;
@@ -11,7 +11,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:aique127.github.io.git master:gh-pages
+git push -f git@github.com:aique127/aique127.github.io master:gh-pages
 
 cd -
 
